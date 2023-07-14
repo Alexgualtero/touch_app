a5966c0f955a95c75b7b6045db0d8708c18ccc32
Alex Gualtero <alexandergualtero@hotmail.com>
2023-07-13 21:54:05 -0500

    cambio

---
+++ src/login.jsx
@@ -0,0 +1,9 @@
+export function Login({titulo1, titulo2}){
+    return (
+    <div>
+          <h1 className='title1'>{titulo1}</h1>
+          <div>{titulo2}</div>
+          <div>HOLA MUNDO</div>
+    </div>
+    );
+  }
